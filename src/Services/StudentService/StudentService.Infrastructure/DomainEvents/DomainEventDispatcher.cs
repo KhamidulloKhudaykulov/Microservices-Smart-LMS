d3ce43082b950f37f84fe19f8059aae6abf02d9e
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-22 15:00:53 +0500

    Implemented DomainEventDispatcher

---
+++ src/Services/StudentService/StudentService.Infrastructure/DomainEvents/DomainEventDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using StudentService.Application.Interfaces.DomainEvents;
+using StudentService.Domain.Events;
+
+namespace StudentService.Infrastructure.DomainEvents;
+
+public class DomainEventDispatcher : IDomainEventDispatcher
+{
+    public Task DispatchAsync(IReadOnlyCollection<IDomainEvent> domainEvents)
+    {
+        throw new NotImplementedException();
+    }
+}
