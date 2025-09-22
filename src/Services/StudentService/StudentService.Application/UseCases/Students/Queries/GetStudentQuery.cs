3efe52643251df89c8eec77a4bda87451d42ca7b
Asrorbek Abrorov <as.abrorov@gmail.com>
2025-09-23 00:39:00 +0500

    GetStudentQuery.cs file added

---
+++ src/Services/StudentService/StudentService.Application/UseCases/Students/Queries/GetStudentQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using StudentService.Application.UseCases.Students.Contracts;
+
+namespace StudentService.Application.UseCases.Students.Queries;
+
+public record GetStudentQuery() : IRequest<Result<IEnumerable<StudentResponseDto>>>;
+    
+public class GetStudentQueryHandler : IRequestHandler<GetStudentQuery, Result<IEnumerable<StudentResponseDto>>>
+{
+    
+    public Task<Result<IEnumerable<StudentResponseDto>>> Handle(GetStudentQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
