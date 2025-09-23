3af5eaaa4dcc0c43c073ea3630d82a89dbbc3648
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-23 09:26:39 +0500

    Created DomainEvents

---
+++ src/Services/PaymentService/PaymentService.Domain/DomainEvents/Payments/PaymentCompletedDomainEvent.cs
@@ -0,0 +1,26 @@
+﻿using PaymentService.Domain.Enums;
+using PaymentService.Domain.Events;
+
+namespace PaymentService.Domain.DomainEvents.Payments;
+
+public class PaymentCompletedDomainEvent : DomainEvent
+{
+    public Guid UserId { get; set; }
+    public Guid CourseId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime PaymentDate { get; set; }
+    public List<Month>? ForMonths { get; set; }
+    public PaymentCompletedDomainEvent(
+        Guid userId,
+        Guid courseId,
+        decimal amount,
+        DateTime paymentDate,
+        List<Month>? forMonths)
+    {
+        UserId = userId;
+        CourseId = courseId;
+        Amount = amount;
+        PaymentDate = paymentDate;
+        ForMonths = forMonths;
+    }
+}
---
+++ src/Services/PaymentService/PaymentService.Domain/DomainEvents/Payments/PaymentRefundedDomainEvent.cs
@@ -0,0 +1,23 @@
+﻿namespace PaymentService.Domain.DomainEvents.Payments;
+
+public class PaymentRefundedDomainEvent
+{
+    public Guid UserId { get; set; }
+    public Guid CourseId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime RefundDate { get; set; }
+    public List<Enums.Month>? ForMonths { get; set; }
+    public PaymentRefundedDomainEvent(
+        Guid userId,
+        Guid courseId,
+        decimal amount,
+        DateTime refundDate,
+        List<Enums.Month>? forMonths)
+    {
+        UserId = userId;
+        CourseId = courseId;
+        Amount = amount;
+        RefundDate = refundDate;
+        ForMonths = forMonths;
+    }
+}
