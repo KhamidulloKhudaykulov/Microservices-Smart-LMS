29942842e7d1d1dabd0f33dffc3feab53ebd42f9
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-23 10:06:39 +0500

    Created Rule For CreateCoursePaymentCommandHandler

---
+++ src/Services/PaymentService/PaymentService.Application/UseCases/Payments/Commands/Rules/CourseMustExistRule.cs
@@ -0,0 +1,19 @@
+﻿using PaymentService.Application.Common.Rules;
+using PaymentService.Application.Interfaces;
+
+namespace PaymentService.Application.UseCases.Payments.Commands.Rules;
+
+public class CourseMustExistRule(
+    ICourseServiceClient _courseServiceClient) : RuleBase<CreatePaymentCommand>
+{
+    protected override async Task<Result> HandleAsync(CreatePaymentCommand command, CancellationToken cancellationToken)
+    {
+        var course = await _courseServiceClient.IsCourseAvailableAsync(command.CourseId);
+        if (!course)
+            return Result.Failure(new Error(
+                "Course.NotFound", 
+                "The specified course does not exist."));
+
+        return Result.Success();
+    }
+}
---
+++ src/Services/PaymentService/PaymentService.Application/UseCases/Payments/Commands/Rules/UserMustExistRule.cs
@@ -0,0 +1,20 @@
+﻿using PaymentService.Application.Common.Rules;
+using PaymentService.Application.Interfaces;
+
+namespace PaymentService.Application.UseCases.Payments.Commands.Rules;
+
+public class UserMustExistRule(
+    IUserServiceClient _userServiceClient) : RuleBase<CreatePaymentCommand>
+{
+    protected override async Task<Result> HandleAsync(CreatePaymentCommand command, CancellationToken cancellationToken)
+    {
+        var user = await _userServiceClient.IsUserAvailableAsync(command.UserId);
+        if (!user)
+            return Result.Failure(new Error(
+                "User.NotFound", 
+                "The specified user does not exist."));
+
+
+        return Result.Success();
+    }
+}
