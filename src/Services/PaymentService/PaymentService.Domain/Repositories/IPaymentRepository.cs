9cbca5538aa1ea2d2ba5fd1ba03008a4d325d999
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-23 10:07:34 +0500

    Implemented Repository And UoW

---
+++ src/Services/PaymentService/PaymentService.Domain/Repositories/IPaymentRepository.cs
@@ -0,0 +1,11 @@
+﻿using PaymentService.Domain.Entities;
+
+namespace PaymentService.Domain.Repositories;
+
+public interface IPaymentRepository
+{
+    Task<PaymentEntity> InsertAsync(PaymentEntity entity);
+    Task<PaymentEntity?> GetByIdAsync(Guid id);
+    Task<PaymentEntity> UpdateAsync(PaymentEntity entity);
+    IQueryable<PaymentEntity> SelectAllAsQueryable();
+}
---
+++ src/Services/PaymentService/PaymentService.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace PaymentService.Domain.Repositories;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
