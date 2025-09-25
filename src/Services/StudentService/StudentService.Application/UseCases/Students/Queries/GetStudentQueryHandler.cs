4cdd05db33341f405cecc3aee2e5a29265003091
Asrorbek Abrorov <as.abrorov@gmail.com>
2025-09-25 11:56:21 +0500

    GetStudentQueryHandler.cs updated slightly

--- src/Services/StudentService/StudentService.Application/UseCases/Students/Queries/GetStudentQueryHandler.cs
+++ src/Services/StudentService/StudentService.Application/UseCases/Students/Queries/GetStudentQueryHandler.cs
@@ -1,14 +1,27 @@
 ﻿using MediatR;
+using StudentService.Application.Interfaces.Redis;
 using StudentService.Application.UseCases.Students.Contracts;
+using StudentService.Domain.Repositories;
 
 namespace StudentService.Application.UseCases.Students.Queries;
 
 public record GetStudentQuery() : IRequest<Result<IEnumerable<StudentResponseDto>>>;
     
-public class GetStudentQueryHandler : IRequestHandler<GetStudentQuery, Result<IEnumerable<StudentResponseDto>>>
+public class GetStudentQueryHandler: IRequestHandler<GetStudentQuery, Result<IEnumerable<StudentResponseDto>>>
 {
+    private readonly IStudentRepository _studentRepository;
+    private readonly IRedisCacheService _redisCacheService;
+    
+    public GetStudentQueryHandler(IStudentRepository studentRepository, IRedisCacheService redisCacheService)
+    {
+        _studentRepository = studentRepository;
+        _redisCacheService = redisCacheService;
+    }
+    
     public Task<Result<IEnumerable<StudentResponseDto>>> Handle(GetStudentQuery request, CancellationToken cancellationToken)
     {
+        
+        
         throw new NotImplementedException();
     }
 }
