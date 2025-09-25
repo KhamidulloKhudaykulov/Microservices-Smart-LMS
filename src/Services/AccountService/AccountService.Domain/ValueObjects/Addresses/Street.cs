50db5185fd9fe3cab341b5dbfca46d87c918764b
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-25 17:24:48 +0500

    Created ValueObjects For Address entity

---
+++ src/Services/AccountService/AccountService.Domain/ValueObjects/Addresses/Street.cs
@@ -0,0 +1,42 @@
+﻿using SharedKernel.Domain.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountService.Domain.ValueObjects.Addresses;
+
+public class Street : ValueObject
+{
+    private Street(string value) => Value = value;
+
+    public string Value { get; }
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+
+    public static Result<Street> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return Result.Failure<Street>(new Error(
+                code: "Street.Empty",
+                message: "Street cannot be empty"));
+        }
+
+        if (value.Length > 100)
+        {
+            return Result.Failure<Street>(new Error(
+                code: "Street.TooLong",
+                message: "Street name cannot exceed 100 characters"));
+        }
+
+        return Result.Success(new Street(value));
+    }
+
+    public override string ToString() => Value;
+}
+
