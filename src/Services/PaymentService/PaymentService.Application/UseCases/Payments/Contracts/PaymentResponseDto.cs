b1e77015f161d964fa8d8ecacfe19df15af7bfb3
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-25 11:06:12 +0500

    Created PaymentResponseDto For Clearly Send Reponse

---
+++ src/Services/PaymentService/PaymentService.Application/UseCases/Payments/Contracts/PaymentResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using PaymentService.Domain.Enums;
+
+namespace PaymentService.Application.UseCases.Payments.Contracts;
+
+public class PaymentResponseDto
+{
+    public Guid AccountId { get; set; }
+    public Guid UserId { get; set; }
+    public Guid CourseId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime PaymentDate { get; set; }
+    public List<Month>? ForMonths { get; set; }
+    public string? PaymentMethod { get; set; }
+    public string? CancellationReason { get; set; }
+}
