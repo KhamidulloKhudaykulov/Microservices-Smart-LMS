9fafad1945c2aef071cc55f47c913ed08780a209
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-25 17:24:09 +0500

    Created States For AccountEntity

---
+++ src/Services/AccountService/AccountService.Domain/States/Accounts/ClosedAccountState.cs
@@ -0,0 +1,47 @@
+﻿using AccountService.Domain.Entities;
+using AccountService.Domain.Enums;
+using AccountService.Domain.Interfaces;
+using SharedKernel.Domain.Primitives;
+
+namespace AccountService.Domain.States;
+
+public class ClosedAccountState : IAccountStatusState
+{
+    public void Activate(AccountEntity account)
+    {
+        // Yopilgan account faollashmaydi
+        Result.Failure(new Error(
+            code: "Account.Closed",
+            message: "Closed account cannot be activated"));
+    }
+
+    public void Deactivate(AccountEntity account)
+    {
+        // Yopilgan account deaktivalanishi mumkin emas
+        Result.Failure(new Error(
+            code: "Account.Closed",
+            message: "Closed account cannot be deactivated"));
+    }
+
+    public void Suspend(AccountEntity account)
+    {
+        Result.Failure(new Error(
+            code: "Account.Closed",
+            message: "Closed account cannot be suspended"));
+    }
+
+    public void Close(AccountEntity account)
+    {
+        // Allaqachon closed
+        Result.Failure(new Error(
+            code: "Account.AlreadyClosed",
+            message: "This account is already closed"));
+    }
+
+    public void Lock(AccountEntity account)
+    {
+        Result.Failure(new Error(
+            code: "Account.Closed",
+            message: "Closed account cannot be locked"));
+    }
+}
---
+++ src/Services/AccountService/AccountService.Domain/States/Accounts/InactiveAccountState.cs
@@ -0,0 +1,41 @@
+﻿using AccountService.Domain.Entities;
+using AccountService.Domain.Enums;
+using AccountService.Domain.Interfaces;
+using SharedKernel.Domain.Primitives;
+
+namespace AccountService.Domain.States;
+
+public class InactiveAccountState : IAccountStatusState
+{
+    public void Activate(AccountEntity account)
+    {
+        account.SetState(new ActiveAccountState());
+        account.ChangeStatus(AccountStatus.Active);
+    }
+
+    public void Deactivate(AccountEntity account)
+    {
+        // Allaqachon inactive
+        Result.Failure(new Error(
+            code: "Account.AlreadyInactive",
+            message: "This account is already inactive"));
+    }
+
+    public void Suspend(AccountEntity account)
+    {
+        account.SetState(new SuspendedAccountState());
+        account.ChangeStatus(AccountStatus.Suspended);
+    }
+
+    public void Close(AccountEntity account)
+    {
+        account.SetState(new ClosedAccountState());
+        account.ChangeStatus(AccountStatus.Closed);
+    }
+
+    public void Lock(AccountEntity account)
+    {
+        account.SetState(new LockedAccountState());
+        account.ChangeStatus(AccountStatus.Locked);
+    }
+}
