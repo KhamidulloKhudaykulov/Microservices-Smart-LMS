088d20e70e1b5a4715a832917319bbf81eab37be
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-25 17:34:40 +0500

    Added IAccountSpecification For Working Easy With Db

---
+++ src/Services/AccountService/AccountService.Domain/Interfaces/IAccountSpecification.cs
@@ -0,0 +1,9 @@
+﻿using AccountService.Domain.Entities;
+using SharedKernel.Domain.Specifications;
+
+namespace AccountService.Domain.Interfaces;
+
+public interface IAccountSpecification : ISpecification<AccountEntity>
+{
+
+}
