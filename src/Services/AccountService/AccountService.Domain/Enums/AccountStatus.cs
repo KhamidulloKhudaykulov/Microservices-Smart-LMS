c6fb0ce720f97d8abb5610c7430e8718cd5ece8c
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-25 17:23:40 +0500

    Created Enums For Using As Property

---
+++ src/Services/AccountService/AccountService.Domain/Enums/AccountStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountService.Domain.Enums;
+
+public enum AccountStatus
+{
+    Inactive = 0,
+    Active = 1,
+    Suspended = 2,
+    Closed = 3,
+    Pending = 4,
+    Locked = 5
+}
---
+++ src/Services/AccountService/AccountService.Domain/Enums/City.cs
@@ -0,0 +1,19 @@
+﻿namespace AccountService.Domain.Enums;
+
+public enum City
+{
+    Unknown = 0,
+    Tashkent = 1,
+    Andijan = 2,
+    Bukhara = 3,
+    Fergana = 4,
+    Jizzakh = 5,
+    Namangan = 6,
+    Navoi = 7,
+    Kashkadarya = 8,
+    Karakalpakstan = 9,
+    Samarkand = 10,
+    Sirdarya = 11,
+    Surkhandarya = 12,
+    Khorezm = 13
+}
