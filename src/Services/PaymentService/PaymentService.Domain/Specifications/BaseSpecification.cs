33ca21371dbfcc0e05179116ca519e38a8b22b15
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-25 11:04:09 +0500

    Implemented BaseSpecification And PaymentByAccount For Getting Data From Db

---
+++ src/Services/PaymentService/PaymentService.Domain/Specifications/BaseSpecification.cs
@@ -0,0 +1,31 @@
+﻿using PaymentService.Domain.Interfaces;
+using System.Linq.Expressions;
+
+namespace PaymentService.Domain.Specifications;
+
+public abstract class BaseSpecification<T> : ISpecification<T>
+{
+    public Expression<Func<T, bool>>? Criteria { get; protected set; }
+    public List<Expression<Func<T, object>>> Includes { get; } = new();
+    public Expression<Func<T, object>>? OrderBy { get; protected set; }
+    public Expression<Func<T, object>>? OrderByDescending { get; protected set; }
+
+    public bool? IsPagingEnabled => Skip.HasValue && Take.HasValue;
+    public int? Skip { get; protected set; }
+    public int? Take { get; protected set; }
+
+    protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        => Includes.Add(includeExpression);
+
+    protected void ApplyOrderBy(Expression<Func<T, object>> orderByExpression)
+     => OrderBy = orderByExpression;
+
+    protected void ApplyOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        => OrderByDescending = orderByDescExpression;
+
+    protected void ApplyPaging(int skip, int take)
+    {
+        Skip = skip;
+        Take = take;
+    }
+}
---
+++ src/Services/PaymentService/PaymentService.Domain/Specifications/PaymentByAccountSpecification.cs
@@ -0,0 +1,13 @@
+﻿using PaymentService.Domain.Entities;
+
+namespace PaymentService.Domain.Specifications;
+
+public class PaymentByAccountSpecification : BaseSpecification<PaymentEntity>
+{
+    public PaymentByAccountSpecification(Guid accountId, int pageNumber, int pageSize)
+    {
+        Criteria = p => p.AccountId == accountId;
+        ApplyOrderBy(p => p.PaymentDate);
+        ApplyPaging((pageNumber - 1) * pageSize, pageSize);
+    }
+}
