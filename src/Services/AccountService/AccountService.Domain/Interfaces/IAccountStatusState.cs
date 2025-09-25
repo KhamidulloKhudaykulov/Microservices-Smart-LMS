db56a748bb2f03a0861cd787552a99e630038a3d
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-25 17:23:58 +0500

    Created IAccountStatusState For Implementing State Pattern

---
+++ src/Services/AccountService/AccountService.Domain/Interfaces/IAccountStatusState.cs
@@ -0,0 +1,13 @@
+﻿using AccountService.Domain.Entities;
+using SharedKernel.Domain.Primitives;
+
+namespace AccountService.Domain.Interfaces;
+
+public interface IAccountStatusState
+{
+    void Activate(AccountEntity account);
+    void Deactivate(AccountEntity account);
+    void Suspend(AccountEntity account);
+    void Close(AccountEntity account);
+    void Lock(AccountEntity account);
+}
