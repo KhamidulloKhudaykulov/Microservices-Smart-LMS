04f49c4eb31bf3fb1ae9eb13108e8cf84772a57d
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-26 17:27:23 +0500

    Created DTO For Resonse Account Entity

---
+++ src/Services/AccountService/AccountService.Application/UseCases/AccountSettings/Contracts/AccountSettingResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using AccountService.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountService.Application.UseCases.AccountSettings.Contracts;
+
+public class AccountSettingResponseDto
+{
+    public Guid Id { get; set; }
+    public Guid AccountId { get; set; }
+    public string Status { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public bool IsActived { get; set; }
+}
