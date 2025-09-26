4280d16928a2514e40fa4ae02a5495e4bffdd53a
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-26 11:26:20 +0500

    Created AttachSettingsToAccount Command Handler

---
+++ src/Services/AccountService/AccountService.Application/UseCases/AccountSettings/Commands/AttachSettingsToAccountCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AccountService.Domain.Enums;
+using AccountService.Domain.Repsoitories;
+using MediatR;
+
+namespace AccountService.Application.UseCases.AccountSettings.Commands;
+
+public record AttachSettingsToAccountCommand(
+    Guid AccountId,
+    AccountStatus AccountStatus) : IRequest<Result>;
+
+
+public class AttachSettingsToAccountCommandHandler(
+    IAccountRepository _accountRepository,
+    IUnitOfWork _unitOfWork) 
+    : IRequestHandler<AttachSettingsToAccountCommand, Result>
+{
+    public async Task<Result> Handle(AttachSettingsToAccountCommand request, CancellationToken cancellationToken)
+    {
+        var account = await _accountRepository.SelectByIdAsync(request.AccountId);
+        if (account is null)
+            return Result.Failure(new Error(
+                code: "Account.NotFound",
+                message: "Account not found"));
+
+        var settings = account.AttachAccountSetting(request.AccountStatus);
+
+        await _accountRepository.UpdateAsync(account);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
