4b082c920a4b9d2e1eb6d178033ede28b51aa595
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-26 17:27:02 +0500

    Created Some Rules Based On CoR

---
+++ src/Services/AccountService/AccountService.Application/UseCases/Accounts/Rules/CreateAccountMustSuccessRule.cs
@@ -0,0 +1,44 @@
+﻿using AccountService.Application.UseCases.Accounts.Commands;
+using AccountService.Domain.Aggregates;
+using AccountService.Domain.Repositories;
+using AccountService.Domain.Specifications.Accounts;
+using AccountService.Domain.ValueObjects.Accounts;
+using SharedKernel.Application.Common.Rules;
+
+namespace AccountService.Application.UseCases.Accounts.Rules;
+
+public class CreateAccountMustSuccessRule(
+    IAccountRepository _accountRepository)
+    : RuleBase<CreateAccountCommand>
+{
+    protected override async Task<Result> HandleAsync(
+        CreateAccountCommand command,
+        CancellationToken cancellationToken)
+    {
+        var byPhoneNumberSpec = new AccountByPhoneNumberSpecification(command.PhoneNumber);
+        var byEmailSpec = new AccountByEmailSpecification(command.Email);
+
+        var existingAccountWithPhoneNumber = await _accountRepository.SelectAsync(byPhoneNumberSpec);
+        var existingAccountWithEmail = await _accountRepository.SelectAsync(byEmailSpec);
+
+        if (existingAccountWithPhoneNumber is not null
+            || existingAccountWithEmail is not null)
+        {
+            return Result.Failure(new Error(
+                code: "Account.AlreadyExists",
+                message: "An account with the provided phone number or email already exists."));
+        }
+        
+        var account = AccountEntity.Create(
+            AccountName.Create(command.Name).Value, 
+            PhoneNumber.Create(command.PhoneNumber).Value, 
+            Email.Create(command.Email).Value);
+
+        if (account.IsFailure)
+            return Result.Failure(account.Error);
+
+        await _accountRepository.InsertAsync(account.Value);
+
+        return Result.Success(account.Value);
+    }
+}
---
+++ src/Services/AccountService/AccountService.Application/UseCases/Accounts/Rules/UpdateAccountMustBeSuccess.cs
@@ -0,0 +1,63 @@
+﻿using AccountService.Application.UseCases.Accounts.Commands;
+using AccountService.Domain.Aggregates;
+using AccountService.Domain.Repositories;
+using AccountService.Domain.Specifications.Accounts;
+using AccountService.Domain.ValueObjects.Accounts;
+using MediatR;
+using SharedKernel.Application.Common.Rules;
+
+namespace AccountService.Application.UseCases.Accounts.Rules;
+
+public class UpdateAccountMustBeSuccess(
+    IAccountRepository _accountRepository) 
+    : RuleBase<UpdateAccountCommand>
+{
+    protected override async Task<Result> HandleAsync(UpdateAccountCommand command, CancellationToken cancellationToken)
+    {
+        var accountSpecification = new AccountByIdSpecification(command.AccountId);
+
+        var account = await _accountRepository
+            .SelectAsync(accountSpecification);
+
+        if (account is null)
+            return Result.Failure(new Error(
+                code: "Account.NotFound",
+                message: "Account not found"));
+
+        Result<AccountName>? accountNameResult = null;
+        Result<PhoneNumber>? phoneNumberResult = null;
+        Result<Email>? emailResult = null;
+
+        if (!string.IsNullOrEmpty(command.Accountname))
+        {
+            accountNameResult = AccountName.Create(command.Accountname);
+            if (!accountNameResult.IsSuccess)
+                return await Task.FromResult(Result.Failure(accountNameResult.Error));
+        }
+
+        if (!string.IsNullOrEmpty(command.PhoneNumber))
+        {
+            phoneNumberResult = PhoneNumber.Create(command.PhoneNumber);
+            if (!phoneNumberResult.IsSuccess)
+                return await Task.FromResult(Result.Failure(phoneNumberResult.Error));
+        }
+
+        if (!string.IsNullOrEmpty(command.Email))
+        {
+            emailResult = Email.Create(command.Email);
+            if (!emailResult.IsSuccess)
+                return await Task.FromResult(Result.Failure(emailResult.Error));
+        }
+
+        // Entity update
+        account.Update(
+            accountNameResult?.Value,
+            phoneNumberResult?.Value,
+            emailResult?.Value
+        );
+
+        await _accountRepository.UpdateAsync(account);
+
+        return await Task.FromResult(Result.Success());
+    }
+}
