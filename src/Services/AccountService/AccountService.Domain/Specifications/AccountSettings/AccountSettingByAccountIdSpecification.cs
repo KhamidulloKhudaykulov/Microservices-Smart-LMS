64254efd90a45e19d93f1fce294fba1b77dc2407
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-26 11:25:23 +0500

    Implemented Some Specifications

---
+++ src/Services/AccountService/AccountService.Domain/Specifications/AccountSettings/AccountSettingByAccountIdSpecification.cs
@@ -0,0 +1,11 @@
+﻿using AccountService.Domain.Entities;
+
+namespace AccountService.Domain.Specifications.AccountSettings;
+
+public class AccountSettingByAccountIdSpecification : BaseSpecification<AccountSetting>
+{
+    public AccountSettingByAccountIdSpecification(Guid accountId)
+    {
+        Criteria = setting => setting.AccountId == accountId;
+    }
+}
---
+++ src/Services/AccountService/AccountService.Domain/Specifications/Accounts/AccountByIdSpecification.cs
@@ -0,0 +1,15 @@
+﻿using AccountService.Domain.Aggregates;
+
+namespace AccountService.Domain.Specifications.Accounts;
+
+public class AccountByIdSpecification : BaseSpecification<AccountEntity>
+{
+    public AccountByIdSpecification(Guid id)
+    {
+        Criteria = account => account.Id == id;
+
+        AddInclude(a => a.AccountSetting);
+
+        AddInclude(a => a.Address);
+    }
+}
