1e2c8630a205b903162f963d966276c67e2a3794
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-29 16:17:02 +0500

    Created Some QueryHandlers For Getting Information About Lesson

---
+++ src/Services/Education/Modules/LessonModule/LessonModule.Application/UseCases/Lessons/Queries/GetLessonQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using LessonModule.Application.UseCases.Lessons.Contracts;
+using LessonModule.Domain.Exceptions;
+using LessonModule.Domain.Repositories;
+using SharedKernel.Application.Abstractions.Messaging;
+
+namespace LessonModule.Application.UseCases.Lessons.Queries;
+
+public record GetLessonQuery(
+    Guid LessonId) : IQuery<LessonResponseDto>;
+
+public class GetLessonQueryHandler(
+    ILessonRepository _lessonRepository) 
+    : IQueryHandler<GetLessonQuery, LessonResponseDto>
+{
+    public async Task<Result<LessonResponseDto>> Handle(GetLessonQuery request, CancellationToken cancellationToken)
+    {
+        var existLesson = await _lessonRepository.SelectByIdAsync(request.LessonId);
+        if (existLesson is null)
+        {
+            return Results.NotFoundException<LessonResponseDto>(LessonErrors.NotFound);
+        }
+
+        var result = new LessonResponseDto(
+            CourseId: existLesson.CourseId,
+            LessonId: existLesson.Id,
+            StartsAt: existLesson.StartTime);
+
+        return Result.Success(result);
+    }
+}
---
+++ src/Services/Education/Modules/LessonModule/LessonModule.Application/UseCases/Lessons/Queries/GetLessonsByCourseQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using CourseModule.Application.Interfaces;
+using LessonModule.Application.UseCases.Lessons.Contracts;
+using LessonModule.Domain.Exceptions;
+using LessonModule.Domain.Repositories;
+using LessonModule.Domain.Specifications.Lessons;
+using SharedKernel.Application.Abstractions.Messaging;
+
+namespace LessonModule.Application.UseCases.Lessons.Queries;
+
+public record GetLessonsByCourseQuery(
+    Guid CourseId) : IQuery<List<LessonResponseDto>>;
+
+public class GetLessonsByCourseQueryHandler(
+    ILessonRepository _lessonRepository) 
+    : IQueryHandler<GetLessonsByCourseQuery, List<LessonResponseDto>>
+{
+    public async Task<Result<List<LessonResponseDto>>> Handle(GetLessonsByCourseQuery request, CancellationToken cancellationToken)
+    {
+        var lessonSpecification = new LessonByCourseIdSpecification(request.CourseId);
+
+        var lessons = await _lessonRepository.ListAsync(lessonSpecification);
+        if (lessons.Count == 0)
+            return Results.NotFoundException<List<LessonResponseDto>>(LessonErrors.EmptyList);
+
+        var result = lessons
+            .Select(l => new LessonResponseDto(
+                l.CourseId,
+                l.Id,
+                l.StartTime))
+            .ToList();
+
+        return Result.Success(result);
+    }
+}
