b08bbcb3efcf534197c205c6fab8b1e476c4e560
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-29 09:52:44 +0500

    Created Dto For Sending Response of Account Data

---
+++ src/Services/AccountService/AccountService.Application/UseCases/Accounts/Contracts/AccountDataResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountService.Application.UseCases.Accounts.Contracts;
+
+public record AccountDataResponseDto(
+    Guid Id,
+    string AccountName,
+    string PhoneNumber,
+    string Email,
+    string? Address);
