37f41cb25f79f27f3150a1071d6d5cc33313d717
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-29 11:21:51 +0500

    Created Abstaction ILessonRepository

---
+++ src/Services/Education/Modules/LessonModule/LessonModule.Domain/Repositories/ILessonRepository.cs
@@ -0,0 +1,8 @@
+﻿using LessonModule.Domain.Entities;
+using SharedKernel.Domain.Repositories;
+
+namespace LessonModule.Domain.Repositories;
+
+public interface ILessonRepository : IRepository<LessonEntity>
+{
+}
