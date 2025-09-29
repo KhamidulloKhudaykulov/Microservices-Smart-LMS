6c38abaa33541ee7843166b1936503399ae643d1
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-29 16:17:43 +0500

    Implemented Abstraction For Specificaitons

---
+++ src/Services/Education/Modules/LessonModule/LessonModule.Domain/Interfaces/ILessonSpecification.cs
@@ -0,0 +1,8 @@
+﻿using LessonModule.Domain.Entities;
+using SharedKernel.Domain.Specifications;
+
+namespace LessonModule.Domain.Interfaces;
+
+public interface ILessonSpecification : ISpecification<LessonEntity>
+{
+}
