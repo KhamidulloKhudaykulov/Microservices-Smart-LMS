0540b08f2ff1ccd462b23001c24c6beb17140689
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-29 16:41:33 +0500

    Created LesssonsController With Minimal Endpoints

---
+++ src/Services/Education/Modules/Education.Api/Controllers/Lessons/LessonsController.cs
@@ -0,0 +1,39 @@
+﻿using LessonModule.Application.UseCases.Lessons.Commands;
+using LessonModule.Application.UseCases.Lessons.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Education.Api.Controllers.Lessons;
+
+[Route("api/lessons")]
+public class LessonsController : BaseController
+{
+    private readonly ISender _sender;
+
+    public LessonsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateLesson(CreateLessonCommand command)
+    {
+        var result = await _sender.Send(command);
+        return FromResult(result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var query = new GetLessonQuery(id);
+        var result = await _sender.Send(query);
+        return FromResult(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] GetLessonsByCourseQuery query)
+    {
+        var result = await _sender.Send(query);
+        return FromResult(result);
+    }
+}
