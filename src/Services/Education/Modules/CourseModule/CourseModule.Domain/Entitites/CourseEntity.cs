b076af96b78f6035072a6a750c006b5b1d6036ae
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-30 16:28:35 +0500

    Integrated Course Entity With StudentPayment Entity

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Domain/Entitites/CourseEntity.cs
@@ -0,0 +1,33 @@
+﻿using CourseModule.Domain.Enums;
+using CourseModule.Domain.Interfaces;
+using CourseModule.Domain.States.Courses;
+using SharedKernel.Domain.Primitives;
+
+namespace CourseModule.Domain.Entitites;
+
+public class CourseEntity : AggregateRoot
+{
+    public CourseEntity(Guid id) 
+        : base(id) { }
+
+    public string Name { get; set; }
+    public DateTime StartsAt { get; set; }
+    public DateTime EndsAt { get; set; }
+    public int Duration { get; set; }
+    public CourseStatus Status { get; set; }
+
+    private ICourseStatusState _courseStatusState = new OpenCourseState();
+
+    public IList<StudentPayment> StudentPayments { get; set; }
+    public List<Guid> StudentIds { get; set; }
+
+    public void ChangeStatus(CourseStatus status)
+        => Status = status;
+
+    public void SetState(ICourseStatusState courseStatusState)
+        => _courseStatusState = courseStatusState;
+
+    public void Open() => _courseStatusState.Open(this);
+    public void Close() => _courseStatusState.Close(this);
+    public void Block() => _courseStatusState.Block(this);
+}
---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Domain/Entitites/StudentPayment.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseModule.Domain.Entitites;
+
+public class StudentPayment
+{
+    public Guid Id { get; set; }
+    public Guid StudentId { get; set; }
+
+    public List<Guid>? PaymentIds { get; set; }
+}
