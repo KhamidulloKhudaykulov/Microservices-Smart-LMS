76369d993d7b82a6a63593c864ed24a0c265c130
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-09-30 16:28:03 +0500

    created Interface for Implementing State Pattern

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Domain/Interfaces/ICourseStatusState.cs
@@ -0,0 +1,10 @@
+﻿using CourseModule.Domain.Entitites;
+
+namespace CourseModule.Domain.Interfaces;
+
+public interface ICourseStatusState
+{
+    void Open(CourseEntity course);
+    void Close(CourseEntity course);
+    void Block(CourseEntity course);
+}
