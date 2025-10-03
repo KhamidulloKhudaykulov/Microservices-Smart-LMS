1537d97324ed4c9d7f73fa0868e65b908b4f8dd3
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 15:12:09 +0500

    Added Controllers As Partial Classes for Working With Courses

---
+++ src/Services/Education/Modules/Education.Api/Controllers/Courses/CourseController.cs
@@ -0,0 +1,48 @@
+﻿using CourseModule.Application.UseCases.Courses.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Education.Api.Controllers.Courses;
+
+[Route("api/courses")]
+public class CourseController : BaseController
+{
+    private readonly ISender _sender;
+
+    public CourseController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCourse(CreateCourseCommand command)
+    {
+        var response = await _sender.Send(command);
+        if (response.IsFailure)
+            return BadRequest(response.Error.Message);
+
+        return FromResult(response);
+    }
+
+    [HttpPut("{courseId}/name/{name}")]
+    public async Task<IActionResult> UpdateCourseName(Guid courseId, string name)
+    {
+        var command = new UpdateCourseNameCommand(courseId, name);
+        var response = await _sender.Send(command);
+        if (response.IsFailure)
+            return BadRequest(response.Error.Message);
+
+        return FromResult(response);
+    }
+
+    [HttpPut("{courseId}/startsat/{startsAt}")]
+    public async Task<IActionResult> UpdateCourseStartDate(Guid courseId, DateTime startsAt)
+    {
+        var command = new UpdateCourseStartDateCommand(courseId, startsAt);
+        var response = await _sender.Send(command);
+        if (response.IsFailure)
+            return BadRequest(response.Error.Message);
+
+        return FromResult(response);
+    }
+}
---
+++ src/Services/Education/Modules/Education.Api/Controllers/Courses/CourseStatusesController.cs
@@ -0,0 +1,30 @@
+﻿using CourseModule.Application.UseCases.Courses.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Education.Api.Controllers.Courses;
+
+[Route("api/courses")]
+public partial class CourseController
+{
+    [HttpPut("close/{courseId}")]
+    public async Task<IActionResult> Close(Guid courseId)
+    {
+        var command = new CloseCourseCommand(courseId);
+        var response = await _sender.Send(command);
+        if (response.IsFailure)
+            return BadRequest(response.Error.Message);
+
+        return FromResult(response);
+    }
+
+    [HttpPut("block/{courseId}")]
+    public async Task<IActionResult> Block(Guid courseId)
+    {
+        var command = new BlockCourseCommand(courseId);
+        var response = await _sender.Send(command);
+        if (response.IsFailure)
+            return BadRequest(response.Error.Message);
+
+        return FromResult(response);
+    }
+}
