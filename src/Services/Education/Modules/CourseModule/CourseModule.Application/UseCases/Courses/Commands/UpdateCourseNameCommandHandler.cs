15e67956a4ad64190dd73b336ee24353bc66e0d2
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 10:05:21 +0500

    Added Command Handler For Updating Some Colums For Course Entity

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Application/UseCases/Courses/Commands/UpdateCourseNameCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CourseModule.Domain.Exceptions;
+using CourseModule.Domain.Repositories;
+using MediatR;
+using SharedKernel.Application.Abstractions.Messaging;
+using SharedKernel.Domain.Repositories;
+
+namespace CourseModule.Application.UseCases.Courses.Commands;
+
+public record UpdateCourseNameCommand(
+    Guid CourseId,
+    string CourseName) : ICommand<Unit>;
+
+public class UpdateCourseNameCommandHandler(
+    ICourseRepository _courseRepository,
+    IUnitOfWork _unitOfWork) : ICommandHandler<UpdateCourseNameCommand, Unit>
+{
+    public async Task<Result<Unit>> Handle(UpdateCourseNameCommand request, CancellationToken cancellationToken)
+    {
+        var existCourse = await _courseRepository
+            .SelectByIdAsync(request.CourseId);
+
+        if (existCourse is null)
+            return Results.NotFoundException<Unit>(CourseErrors.NotFound);
+
+        existCourse.UpdateCourseName(request.CourseName);
+
+        await _courseRepository.UpdateAsync(existCourse);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(Unit.Value);
+    }
+}
---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Application/UseCases/Courses/Commands/UpdateCourseStartDateCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using CourseModule.Domain.Exceptions;
+using CourseModule.Domain.Repositories;
+using MediatR;
+using SharedKernel.Application.Abstractions.Messaging;
+using SharedKernel.Domain.Repositories;
+
+namespace CourseModule.Application.UseCases.Courses.Commands;
+
+public record UpdateCourseStartDateCommand(
+    Guid CourseId,
+    DateTime StartsAt) : ICommand<Unit>;
+
+public class UpdateCourseStartDateCommandHandler(
+    ICourseRepository _courseRepository,
+    IUnitOfWork _unitOfWork) 
+    : ICommandHandler<UpdateCourseStartDateCommand, Unit>
+{
+    public async Task<Result<Unit>> Handle(UpdateCourseStartDateCommand request, CancellationToken cancellationToken)
+    {
+        var existCourse = await _courseRepository
+            .SelectByIdAsync(request.CourseId);
+
+        if (existCourse is null)
+            return Results.NotFoundException<Unit>(CourseErrors.NotFound);
+
+        existCourse.UpdateStartDate(request.StartsAt);
+
+        await _courseRepository.UpdateAsync(existCourse);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(Unit.Value);
+    }
+}
