91786d43219a4d3f8b0d034cb809c9083f5f18f2
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 10:03:33 +0500

    Added Default Course Errors As Static String

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Domain/Exceptions/CourseErrors.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseModule.Domain.Exceptions;
+
+public static class CourseErrors
+{
+    public const string NotFound = "This course was not found";
+    public const string EmptyList = "Empty courses list";
+    public const string AlreadyExists = "This course has already created";
+    public const string InvalidDate = "Date invalid";
+}
