f4d9157ff0b227e89311e67ac8bf70d1eda88a90
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 15:14:46 +0500

    Modified API Layer

--- src/Services/Education/Modules/Education.Api/Program.cs
+++ src/Services/Education/Modules/Education.Api/Program.cs
@@ -1,12 +1,15 @@
+using Education.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddModules(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
