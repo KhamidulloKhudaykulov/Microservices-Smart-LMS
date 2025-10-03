185e9da2b1d0f9f544a8b14b37301ae007db263f
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 10:27:17 +0500

    Added Ef Configuration For Course Entity

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Infrastructure/EfConfigurations/CourseEntityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using CourseModule.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CourseModule.Infrastructure.EfConfigurations;
+
+public class CourseEntityConfiguration : IEntityTypeConfiguration<CourseEntity>
+{
+    public void Configure(EntityTypeBuilder<CourseEntity> builder)
+    {
+        builder.ToTable("courses");
+    }
+}
