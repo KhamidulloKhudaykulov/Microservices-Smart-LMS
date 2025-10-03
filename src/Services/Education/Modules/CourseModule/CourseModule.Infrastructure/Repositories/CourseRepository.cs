a04dfbb608bd2d11274a5a8dec3bc307d14ec6f6
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 15:34:48 +0500

    Implemented Repository And UnitOfWork

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Infrastructure/Repositories/CourseRepository.cs
@@ -0,0 +1,67 @@
+﻿using CourseModule.Domain.Entitites;
+using CourseModule.Domain.Repositories;
+using CourseModule.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseModule.Infrastructure.Repositories;
+
+public class CourseRepository : ICourseRepository
+{
+    private readonly CourseDbContext _dbContext;
+    private readonly DbSet<CourseEntity> _courses;
+
+    public CourseRepository(CourseDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _courses = _dbContext.Set<CourseEntity>();
+    }
+
+    public Task DeleteAsync(CourseEntity entity)
+    {
+        _courses.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task<CourseEntity> InsertAsync(CourseEntity entity)
+    {
+        return (await _courses.AddAsync(entity)).Entity;
+    }
+
+    public IQueryable<CourseEntity> SelectAllAsQueryable()
+    {
+        return _courses.AsQueryable();
+    }
+
+    public async Task<IEnumerable<CourseEntity>> SelectAllAsync()
+    {
+        return await _courses.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<IEnumerable<CourseEntity>> SelectAllByAccountIdAsync(Guid accountId)
+    {
+        return await _courses
+            .AsNoTracking()
+            .Where(c => c.AccountId == accountId)
+            .ToListAsync();
+    }
+
+    public async Task<CourseEntity?> SelectByIdAsync(Guid id)
+    {
+        return await _courses
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task<CourseEntity?> SelectByNameAsync(string name)
+    {
+        return await _courses
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Name == name);
+    }
+
+    public Task<CourseEntity> UpdateAsync(CourseEntity entity)
+    {
+        _courses.Update(entity);
+        return Task.FromResult(entity);
+    }
+}
---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using CourseModule.Infrastructure.Persistence;
+using SharedKernel.Domain.Repositories;
+
+namespace CourseModule.Infrastructure.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly CourseDbContext _dbContext;
+
+    public UnitOfWork(CourseDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
