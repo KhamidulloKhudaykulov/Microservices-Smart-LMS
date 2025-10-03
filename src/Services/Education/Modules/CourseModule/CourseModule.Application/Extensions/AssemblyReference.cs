de73050750edb50ad5495183a70f3f07c372e7ba
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 15:11:39 +0500

    Added Extensions To Application Layer (CourseModule)

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Application/Extensions/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace CourseModule.Application.Extensions;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CourseModule.Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCourseModuleApplication(
+        this IServiceCollection services)
+    {
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(AssemblyReference.Assembly);
+        });
+
+        return services;
+    }
+}
