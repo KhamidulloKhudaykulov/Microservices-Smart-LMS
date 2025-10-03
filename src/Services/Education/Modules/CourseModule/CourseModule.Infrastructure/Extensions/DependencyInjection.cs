8fe36906f27b09d35391edf0c22157a0f70fcbb3
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 10:28:36 +0500

    Implemented DI Using DependencyInjection Base Class

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using CourseModule.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CourseModule.Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCourseModuleInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<CourseDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultCourseDbConnection"));
+        });
+
+        return services;
+    }
+}
