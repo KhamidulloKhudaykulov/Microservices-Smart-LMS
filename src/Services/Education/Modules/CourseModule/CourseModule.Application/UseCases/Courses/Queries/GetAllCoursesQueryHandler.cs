83e7f751c59cd3abc8d9b3acbb72d76fb86c6842
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 10:05:53 +0500

    Created Two Base Get Commands For Course Entity

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Application/UseCases/Courses/Queries/GetAllCoursesQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using CourseModule.Application.UseCases.Courses.Contracts;
+using CourseModule.Domain.Exceptions;
+using CourseModule.Domain.Repositories;
+using SharedKernel.Application.Abstractions.Messaging;
+
+namespace CourseModule.Application.UseCases.Courses.Queries;
+
+public record GetAllCoursesQuery(
+    Guid accountId) : IQuery<IEnumerable<CourseResponseDto>>;
+
+public class GetAllCoursesQueryHandler(
+    ICourseRepository _courseRepository) 
+    : IQueryHandler<GetAllCoursesQuery, IEnumerable<CourseResponseDto>>
+{
+    public async Task<Result<IEnumerable<CourseResponseDto>>> Handle(GetAllCoursesQuery request, CancellationToken cancellationToken)
+    {
+        var courses = await _courseRepository
+            .SelectAllByAccountIdAsync(request.accountId);
+
+        if (courses is null || !courses.Any())
+            return Results.NotFoundException<IEnumerable<CourseResponseDto>>(
+                CourseErrors.EmptyList);
+
+        var response = courses
+            .Select(c => new CourseResponseDto(
+                Id: c.Id,
+                Name: c.Name,
+                StartsAt: c.StartsAt));
+
+        return Result.Success(response);
+    }
+}
---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Application/UseCases/Courses/Queries/GetCourseByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using CourseModule.Application.UseCases.Courses.Contracts;
+using CourseModule.Domain.Exceptions;
+using CourseModule.Domain.Repositories;
+using SharedKernel.Application.Abstractions.Messaging;
+
+namespace CourseModule.Application.UseCases.Courses.Queries;
+
+public record GetCourseByIdQuery(
+    Guid CourseId) : IQuery<CourseResponseDto>;
+
+public class GetCourseByIdQueryHandler(
+    ICourseRepository _courseRepository) 
+    : IQueryHandler<GetCourseByIdQuery, CourseResponseDto>
+{
+    public async Task<Result<CourseResponseDto>> Handle(GetCourseByIdQuery request, CancellationToken cancellationToken)
+    {
+        var existCourse = await _courseRepository
+            .SelectByIdAsync(request.CourseId);
+
+        if (existCourse is null)
+            return Results.NotFoundException<CourseResponseDto>(
+                CourseErrors.NotFound);
+
+        var resposne = new CourseResponseDto(
+            Id: existCourse.Id,
+            Name: existCourse.Name,
+            StartsAt: existCourse.StartsAt);
+
+        return Result.Success(resposne);
+    }
+}
