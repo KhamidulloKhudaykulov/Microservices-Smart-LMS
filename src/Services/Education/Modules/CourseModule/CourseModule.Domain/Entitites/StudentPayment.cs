622b255630bdf8fddaa27f042b9d78132361cb60
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 10:27:00 +0500

    Removed StudentPayment Entity And Planned Converting To Other Service

--- src/Services/Education/Modules/CourseModule/CourseModule.Domain/Entitites/StudentPayment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CourseModule.Domain.Entitites;
-
-public class StudentPayment
-{
-    public Guid Id { get; set; }
-    public Guid StudentId { get; set; }
-
-    public List<Guid>? PaymentIds { get; set; }
-}
