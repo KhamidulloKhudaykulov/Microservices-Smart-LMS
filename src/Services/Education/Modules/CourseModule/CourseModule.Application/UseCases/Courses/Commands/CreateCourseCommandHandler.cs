362102234e48ae8881f67b7691c943de1e92756f
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 10:04:26 +0500

    Added CommandHandler For Creating Course Entity

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Application/UseCases/Courses/Commands/CreateCourseCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using CourseModule.Domain.Entitites;
+using CourseModule.Domain.Exceptions;
+using CourseModule.Domain.Repositories;
+using MediatR;
+using SharedKernel.Application.Abstractions.Messaging;
+using SharedKernel.Domain.Repositories;
+
+namespace CourseModule.Application.UseCases.Courses.Commands;
+
+public record CreateCourseCommand(
+    Guid AccountId,
+    string CourseName,
+    DateTime StartsAt) : ICommand<Unit>;
+
+public class CreateCourseCommandHandler(
+    ICourseRepository _courseRepository,
+    IUnitOfWork _unitOfWork) 
+    : ICommandHandler<CreateCourseCommand, Unit>
+{
+    public async Task<Result<Unit>> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+    {
+        var existCourse = await _courseRepository.SelectByNameAsync(request.CourseName);
+        if (existCourse is not null)
+            return Results.AlreadyExistsException<Unit>(CourseErrors.AlreadyExists);
+
+        var newCourse = CourseEntity.Create(
+            id: Guid.NewGuid(),
+            accountId: request.AccountId,
+            name: request.CourseName,
+            startsAt: request.StartsAt);
+
+        if (newCourse.IsFailure)
+            return Results.CustomException<Unit>(newCourse.Error);
+
+        await _courseRepository.InsertAsync(newCourse.Value);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(Unit.Value);
+    }
+}
