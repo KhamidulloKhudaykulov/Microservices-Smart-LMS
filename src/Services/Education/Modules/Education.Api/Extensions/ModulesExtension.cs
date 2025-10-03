8ce904214ad63d83277b81096c529cc2d2d98834
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 15:14:26 +0500

    Implemented DI For Modules In Extension Class

---
+++ src/Services/Education/Modules/Education.Api/Extensions/ModulesExtension.cs
@@ -0,0 +1,18 @@
+﻿using CourseModule.Application.Extensions;
+using CourseModule.Infrastructure.Extensions;
+
+namespace Education.Api.Extensions;
+
+public static class ModulesExtension
+{
+    public static IServiceCollection AddModules(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // CourseModule References
+        services.AddCourseModuleApplication();
+        services.AddCourseModuleInfrastructure(configuration);
+
+        return services;
+    }
+}
