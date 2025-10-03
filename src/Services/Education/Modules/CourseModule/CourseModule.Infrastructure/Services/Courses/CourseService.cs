4779f30e8b14584e92cd98b0c3a69c5d9cccf14b
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-03 15:35:19 +0500

    Implemented And Filled Body Of ICourseService's Method

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Infrastructure/Services/Courses/CourseService.cs
@@ -0,0 +1,21 @@
+﻿using CourseModule.Application.Interfaces;
+using CourseModule.Domain.Exceptions;
+using CourseModule.Domain.Repositories;
+
+namespace CourseModule.Infrastructure.Services.Courses;
+
+public class CourseService(
+    ICourseRepository _courseRepository) 
+    : ICourseService
+{
+    public async Task<Result<bool>> IsCourseAvailable(Guid courseId)
+    {
+        var course = await _courseRepository
+            .SelectByIdAsync(courseId);
+
+        if (course is null)
+            return Results.NotFoundException<bool>(CourseErrors.NotFound);
+
+        return Result.Success(true);
+    }
+}
