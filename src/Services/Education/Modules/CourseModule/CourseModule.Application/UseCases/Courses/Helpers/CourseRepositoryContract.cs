05b610d5ce8b8d0be2c54747706406d2659162fa
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-06 15:10:46 +0500

    Created A Contract Class For DRY Principles

---
+++ src/Services/Education/Modules/CourseModule/CourseModule.Application/UseCases/Courses/Helpers/CourseRepositoryContract.cs
@@ -0,0 +1,22 @@
+﻿using CourseModule.Domain.Entitites;
+using CourseModule.Domain.Exceptions;
+using CourseModule.Domain.Repositories;
+using MediatR;
+
+namespace CourseModule.Application.UseCases.Courses.Helpers;
+
+public static class CourseRepositoryContract
+{
+    public static async Task<Result<CourseEntity>> GetCourseOrNotFoundAsync(
+        ICourseRepository _courseRepository,
+        Guid courseId)
+    {
+        var course = await _courseRepository
+            .SelectByIdAsync(courseId);
+
+        if (course is null)
+            return Results.NotFoundException<CourseEntity>(CourseErrors.NotFound);
+
+        return course;
+    }
+}
