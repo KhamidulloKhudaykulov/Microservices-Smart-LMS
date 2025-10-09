26f7f8c0ce606a26d6d14952e757d5ac692e8511
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 10:45:03 +0500

    Added Migration After Update Aggregate

--- src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/Migrations/20251009051019_AddCourseIdProperty.Designer.cs
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/Migrations/20251009051019_AddCourseIdProperty.Designer.cs
@@ -4,6 +4,7 @@ using HomeworkModule.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeworkModule.Infrastructure.Migrations
 {
     [DbContext(typeof(HomeworkDbContext))]
-    partial class HomeworkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251009051019_AddCourseIdProperty")]
+    partial class AddCourseIdProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/Migrations/20251009051019_AddCourseIdProperty.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeworkModule.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCourseIdProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LessonId",
+                table: "homeworks",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CourseId",
+                table: "homeworks",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "homeworks");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LessonId",
+                table: "homeworks",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
