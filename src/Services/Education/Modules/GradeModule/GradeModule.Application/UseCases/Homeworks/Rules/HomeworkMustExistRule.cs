687239f470b6bc42a74370d229f840cfb21e69bf
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 10:43:41 +0500

    Class Has FIlled

--- src/Services/Education/Modules/GradeModule/GradeModule.Application/UseCases/Homeworks/Rules/HomeworkMustExistRule.cs
+++ src/Services/Education/Modules/GradeModule/GradeModule.Application/UseCases/Homeworks/Rules/HomeworkMustExistRule.cs
@@ -1,5 +1,21 @@
-﻿namespace GradeModule.Application.UseCases.Homeworks.Rules;
+﻿using GradeModule.Application.Common.Rules;
+using GradeModule.Application.UseCases.Homeworks.Commands;
+using HomeworkModule.Application.Interfaces;
 
-public class HomeworkMustExistRule
+namespace GradeModule.Application.UseCases.Homeworks.Rules;
+
+public class HomeworkMustExistRule(
+    IHomeworkServiceClient _homeworkServiceClient)
+    : RuleBase<CreateHomeworkGradeCommand>
 {
+    protected override async Task<Result> HandleAsync(CreateHomeworkGradeCommand command, CancellationToken cancellationToken)
+    {
+        var existHomework = await _homeworkServiceClient
+            .CheckExistHomeworkById(command.HomeworkId);
+
+        if (existHomework.IsFailure)
+            return Result.Failure(existHomework.Error);
+
+        return Result.Success();
+    }
 }
