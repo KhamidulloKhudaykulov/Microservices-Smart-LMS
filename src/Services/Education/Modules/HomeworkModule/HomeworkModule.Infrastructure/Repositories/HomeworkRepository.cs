280c408c48079dee61bd980a67b67ee50ea4dfb8
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 09:36:12 +0500

    Implemented Repo And Uow

---
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/Repositories/HomeworkRepository.cs
@@ -0,0 +1,75 @@
+﻿using HomeworkModule.Domain.Aggregates;
+using HomeworkModule.Domain.Repositories;
+using HomeworkModule.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HomeworkModule.Infrastructure.Repositories;
+
+public class HomeworkRepository : IHomeworkRepository
+{
+    private readonly HomeworkDbContext _dbContext;
+    private readonly DbSet<Homework> _homeworks;
+
+    public HomeworkRepository(HomeworkDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _homeworks = _dbContext.Set<Homework>();
+    }
+
+    public async Task<Homework> InsertAsync(Homework homework)
+    {
+        await _homeworks.AddAsync(homework);
+        return homework;
+    }
+
+    public Task<Homework> UpdateAsync(Homework homework)
+    {
+        _homeworks.Update(homework);
+        return Task.FromResult(homework);
+    }
+
+    public async Task DeleteAsync(Homework homework)
+    {
+        _homeworks.Remove(homework);
+        await Task.CompletedTask;
+    }
+
+    public async Task<Homework?> SelectByIdAsync(Guid id)
+    {
+        return await _homeworks
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<Homework?> SelectAsync(Expression<Func<Homework, bool>> predicate)
+    {
+        return await _homeworks
+            .AsNoTracking()
+            .FirstOrDefaultAsync(predicate);
+    }
+
+    public async Task<IEnumerable<Homework>> SelectAllAsync(Expression<Func<Homework, bool>>? predicate = null)
+    {
+        var query = _homeworks
+            .AsNoTracking()
+            .AsQueryable();
+
+        if (predicate is not null)
+            query = query.Where(predicate);
+
+        return await query.ToListAsync();
+    }
+
+    public IQueryable<Homework> SelectAllAsQueryable(Expression<Func<Homework, bool>>? predicate = null)
+    {
+        var query = _homeworks
+            .AsNoTracking()
+            .AsQueryable();
+
+        if (predicate is not null)
+            query = query.Where(predicate);
+
+        return query;
+    }
+}
---
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/Repositories/HomeworkUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using HomeworkModule.Domain.Repositories;
+
+namespace HomeworkModule.Infrastructure.Repositories;
+
+public class HomeworkUnitOfWork : IHomeworkUnitOfWork
+{
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
