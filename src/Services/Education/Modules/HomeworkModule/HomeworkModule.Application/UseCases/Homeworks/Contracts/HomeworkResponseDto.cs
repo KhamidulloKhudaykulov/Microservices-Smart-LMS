260fb3b2e641c50a806ce32473fd303f90a8b90f
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 08:57:07 +0500

    DTO For Getting Information About Homework Entity

---
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Application/UseCases/Homeworks/Contracts/HomeworkResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeworkModule.Application.UseCases.Homeworks.Contracts;
+
+public record HomeworkResponseDto(
+    Guid Id,
+    Guid LessonId,
+    Guid CreatedBy,
+    DateTime CreatedAt,
+    DateTime EndTime,
+    string Title,
+    string? Description,
+    decimal MaxScore,
+    string Status);
