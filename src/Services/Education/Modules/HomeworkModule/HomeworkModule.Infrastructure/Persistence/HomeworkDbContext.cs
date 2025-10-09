726727b7c4533cae71b2fa18c469843c3a701411
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 09:35:29 +0500

    Implemented Db Context

---
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/Persistence/HomeworkDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HomeworkModule.Domain.Aggregates;
+using HomeworkModule.Infrastructure.EfConfigurations;
+using HomeworkModule.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeworkModule.Infrastructure.Persistence;
+
+public class HomeworkDbContext : DbContext
+{
+    public HomeworkDbContext(DbContextOptions<HomeworkDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new HomeworkConfiguration());
+
+        modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+    }
+}
