de44f9089eb62657397dd7fa3434651451f5cd96
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 08:52:42 +0500

    Implemented CoR Pattern

---
+++ src/Services/Education/Modules/GradeModule/GradeModule.Application/Common/Rules/IRule.cs
@@ -0,0 +1,7 @@
+﻿namespace GradeModule.Application.Common.Rules;
+
+public interface IRule<TCommand>
+{
+    Task<Result> CheckAsync(TCommand command, CancellationToken cancellationToken = default);
+    IRule<TCommand> Then(IRule<TCommand> next);
+}
---
+++ src/Services/Education/Modules/GradeModule/GradeModule.Application/Common/Rules/RuleBase.cs
@@ -0,0 +1,21 @@
+﻿namespace GradeModule.Application.Common.Rules;
+
+public abstract class RuleBase<TCommand> : IRule<TCommand>
+{
+    private IRule<TCommand>? _next;
+    public IRule<TCommand> Then(IRule<TCommand> next)
+        => _next = next;
+    public async Task<Result> CheckAsync(TCommand command, CancellationToken cancellationToken = default)
+    {
+        var result = await HandleAsync(command, cancellationToken);
+        if (!result.IsSuccess)
+            return result;
+
+        if (_next is not null)
+            return await _next.CheckAsync(command, cancellationToken);
+
+        return Result.Success();
+    }
+
+    protected abstract Task<Result> HandleAsync(TCommand command, CancellationToken cancellationToken);
+}
