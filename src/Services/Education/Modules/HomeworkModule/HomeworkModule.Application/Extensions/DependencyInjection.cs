7e86dab76d1f9697d492dda0b755185507dfc004
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 09:35:52 +0500

    Extensions For DI

---
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeworkModule.Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddHomeworkModuleApplication(
+        this IServiceCollection services)
+    {
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(AssemblyReference.Assembly);
+        });
+
+        return services;
+    }
+}
