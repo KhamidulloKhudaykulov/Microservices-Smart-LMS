7b8aa1dd04747030df31bb6a244d75013f2935eb
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 08:54:44 +0500

    Created Entity For Grading Homework Task

---
+++ src/Services/Education/Modules/GradeModule/GradeModule.Domain/Enitites/GradeHomeworkEntity.cs
@@ -0,0 +1,50 @@
+﻿using SharedKernel.Domain.Primitives;
+
+namespace GradeModule.Domain.Enitites;
+
+public class GradeHomeworkEntity : Entity
+{
+    private GradeHomeworkEntity(
+        Guid id,
+        Guid studentId,
+        Guid homeworkId,
+        Guid assignedBy,
+        decimal score,
+        string? feedback)
+    {
+        Id = id;
+        StudentId = studentId;
+        HomeworkId = homeworkId;
+        Score = score;
+        AssignedBy = assignedBy;
+        Feedback = feedback;
+
+        AssignedAt = DateTime.UtcNow;
+    }
+    public Guid StudentId { get; private set; }
+    public Guid CourseId { get; private set; }
+    public Guid HomeworkId { get; private set; }
+    public decimal Score { get; private set; }
+    public DateTime AssignedAt { get; private set; }
+    public Guid AssignedBy { get; private set; }
+    public string? Feedback { get; private set; }
+
+    public static Result<GradeHomeworkEntity> Create(
+        Guid id,
+        Guid studentId,
+        Guid homeworkId,
+        Guid assignedBy,
+        decimal score,
+        string? feedback)
+    {
+        var result = new GradeHomeworkEntity(
+            id,
+            studentId,
+            homeworkId,
+            assignedBy,
+            score,
+            feedback);
+
+        return Result.Success(result);
+    }
+}
