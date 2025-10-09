2edcd95162012eceaa74398cc339632e6efdfc06
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 10:45:31 +0500

    Implemented UoW

--- src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/Repositories/HomeworkUnitOfWork.cs
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/Repositories/HomeworkUnitOfWork.cs
@@ -1,11 +1,19 @@
 ﻿using HomeworkModule.Domain.Repositories;
+using HomeworkModule.Infrastructure.Persistence;
 
 namespace HomeworkModule.Infrastructure.Repositories;
 
 public class HomeworkUnitOfWork : IHomeworkUnitOfWork
 {
-    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    private readonly HomeworkDbContext _context;
+
+    public HomeworkUnitOfWork(HomeworkDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _context.SaveChangesAsync(cancellationToken);
     }
 }
