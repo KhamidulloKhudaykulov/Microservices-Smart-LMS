977fd843807ed93855fe7a98a44ef9c809061c53
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 08:55:50 +0500

    Integrated EfConfiguration For GradeHomework Entity

---
+++ src/Services/Education/Modules/GradeModule/GradeModule.Infrastructure/EfConfigurations/GradeHomeworkEntityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using GradeModule.Domain.Enitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GradeModule.Infrastructure.EfConfigurations;
+
+public class GradeHomeworkEntityConfiguration : IEntityTypeConfiguration<GradeHomeworkEntity>
+{
+    public void Configure(EntityTypeBuilder<GradeHomeworkEntity> builder)
+    {
+        builder.ToTable("homework_grades");
+    }
+}
