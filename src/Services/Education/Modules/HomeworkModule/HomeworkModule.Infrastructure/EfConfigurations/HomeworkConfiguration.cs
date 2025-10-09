64a9656ffecafd6765a36d9ad246876e7506ca80
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 09:34:59 +0500

    Added Ef Configuration For Homework Aggregate

---
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/EfConfigurations/HomeworkConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using HomeworkModule.Domain.Aggregates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HomeworkModule.Infrastructure.EfConfigurations;
+
+public class HomeworkConfiguration : IEntityTypeConfiguration<Homework>
+{
+    public void Configure(EntityTypeBuilder<Homework> builder)
+    {
+        builder.ToTable("homeworks");
+    }
+}
---
+++ src/Services/Education/Modules/HomeworkModule/HomeworkModule.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using HomeworkModule.Domain.Repositories;
+using HomeworkModule.Infrastructure.Persistence;
+using HomeworkModule.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeworkModule.Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddHomeworkModuleInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<HomeworkDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("EducationDbConnection"));
+        });
+
+        services.AddScoped<IHomeworkRepository, HomeworkRepository>();
+        services.AddScoped<IHomeworkUnitOfWork, HomeworkUnitOfWork>();
+
+        return services;
+    }
+}
