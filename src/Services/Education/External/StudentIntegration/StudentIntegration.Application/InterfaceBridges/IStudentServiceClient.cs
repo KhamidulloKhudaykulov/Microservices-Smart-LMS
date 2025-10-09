cc7fbebce849303687c9a01f69845b9136b5a0b0
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-09 10:45:42 +0500

    Improved

--- src/Services/Education/External/StudentIntegration/StudentIntegration.Application/InterfaceBridges/IStudentServiceClient.cs
+++ src/Services/Education/External/StudentIntegration/StudentIntegration.Application/InterfaceBridges/IStudentServiceClient.cs
@@ -5,7 +5,7 @@ namespace StudentIntegration.Application.InterfaceBridges;
 public interface IStudentServiceClient
 {
     //Task<bool> CheckExistStudentByIdAsync(Guid studentId);
-    Task<StudentResponseContract> VerifyExistStudentById(Guid studentId);
+    Task<StudentResponseContract?> VerifyExistStudentById(Guid studentId);
     Task<List<StudentResponseContract>> GetStudentsByIds(List<Guid> studentIds);
     Task<StudentResponseContract> GetStudentDetailsById(Guid studentId);
 }
