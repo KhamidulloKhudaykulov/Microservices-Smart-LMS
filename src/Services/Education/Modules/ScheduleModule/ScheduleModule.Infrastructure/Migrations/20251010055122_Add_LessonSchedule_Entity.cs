0996a422ac96f6c87971d15f6973581f14b6b816
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 10:55:04 +0500

    Created Migrations

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Infrastructure/Migrations/20251010055122_Add_LessonSchedule_Entity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScheduleModule.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_LessonSchedule_Entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "schedule_lesson",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LessonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AbsentStudents = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_schedule_lesson", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "schedule_lesson");
+        }
+    }
+}
