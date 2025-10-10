94b7055c5f6cf86f655725399a6d55f6f5690123
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 10:53:56 +0500

    Marking Absent Or Persent Students At Lesson

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Application/UseCases/LessonSchedules/Commands/MarkStudentsAbsentAtLessonCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using ScheduleModule.Domain.Entities;
+using ScheduleModule.Domain.Repositories;
+using SharedKernel.Application.Abstractions.Messaging;
+
+namespace ScheduleModule.Application.UseCases.LessonSchedules.Commands;
+
+public record MarkStudentsAbsentAtLessonCommand(
+    Guid LessonId,
+    List<Guid> StudentIds) : ICommand<Unit>;
+
+public class MarkStudentsAbsentAtLessonCommandHandler(
+    IScheduleRepository<LessonScheduleEntity> _scheduleRepository,
+    IScheduleUnitOfWork _unitOfWork)
+    : ICommandHandler<MarkStudentsAbsentAtLessonCommand, Unit>
+{
+    public async Task<Result<Unit>> Handle(MarkStudentsAbsentAtLessonCommand request, CancellationToken cancellationToken)
+    {
+        var existSchedule = await _scheduleRepository
+            .SelectAsync(e => e.LessonId == request.LessonId);
+
+        if (existSchedule is not null)
+        {
+            existSchedule.MarkStudentsAbsent(request.StudentIds);
+
+            await _scheduleRepository.UpdateAsync(existSchedule);
+        }
+        else
+        {
+            var entity = LessonScheduleEntity.Create(request.LessonId).Value;
+            entity.MarkStudentsAbsent(request.StudentIds);
+
+            await _scheduleRepository.InsertAsync(entity);
+        }
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(Unit.Value);
+    }
+}
