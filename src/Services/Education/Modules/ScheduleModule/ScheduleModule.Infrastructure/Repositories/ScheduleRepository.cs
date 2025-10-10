0728928a9a8b47c11aa09d67d33a1e1e4020ce1b
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 10:55:40 +0500

    Implemented Repositories

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Infrastructure/Repositories/ScheduleRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScheduleModule.Domain.Repositories;
+using ScheduleModule.Infrastructure.Persistence;
+using SharedKernel.Domain.Primitives;
+using System.Linq.Expressions;
+
+namespace ScheduleModule.Infrastructure.Repositories;
+
+public class ScheduleRepository<T> : IScheduleRepository<T> 
+    where T : Entity
+{
+    private readonly ScheduleDbContext _dbContext;
+    private readonly DbSet<T> _dbSet;
+
+    public ScheduleRepository(ScheduleDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<T>();
+    }
+
+    public async Task<T> InsertAsync(T entity)
+    {
+        var entry = await _dbSet.AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+        return entry.Entity;
+    }
+
+    public async Task<T> UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await _dbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<T?> SelectByIdAsync(Guid id)
+    {
+        // T entity ning Id propertysi bo'lishi shart
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<T?> SelectAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.FirstOrDefaultAsync(predicate);
+    }
+
+    public IQueryable<T> SelectAllAsQueryable(Expression<Func<T, bool>>? predicate = null)
+    {
+        IQueryable<T> query = _dbSet.AsQueryable();
+        if (predicate != null)
+            query = query.Where(predicate);
+        return query;
+    }
+
+    public async Task<IEnumerable<T>> SelectAllAsync(Expression<Func<T, bool>>? predicate = null)
+    {
+        IQueryable<T> query = _dbSet.AsQueryable();
+        if (predicate != null)
+            query = query.Where(predicate);
+        return await query.ToListAsync();
+    }
+}
---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Infrastructure/Repositories/ScheduleUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using ScheduleModule.Domain.Repositories;
+using ScheduleModule.Infrastructure.Persistence;
+
+namespace ScheduleModule.Infrastructure.Repositories;
+
+public class ScheduleUnitOfWork : IScheduleUnitOfWork
+{
+    private readonly ScheduleDbContext _dbContext;
+
+    public ScheduleUnitOfWork(ScheduleDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
