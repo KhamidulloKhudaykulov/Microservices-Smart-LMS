a956cab7423abce3a2444109ed689b59db1ef8e8
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 10:53:27 +0500

    Extension DI

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ScheduleModule.Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddScheduleModuleApplication(
+        this IServiceCollection services)
+    {
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(AssemblyReference.Assembly);
+        });
+
+        return services;
+    }
+}
