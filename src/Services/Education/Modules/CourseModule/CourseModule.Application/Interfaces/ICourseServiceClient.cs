161fd3564bc4ca2c276a8c3fc5d7ef66f3c0847b
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 12:16:02 +0500

    Method For Getting Infomation With Ex Func

--- src/Services/Education/Modules/CourseModule/CourseModule.Application/Interfaces/ICourseServiceClient.cs
+++ src/Services/Education/Modules/CourseModule/CourseModule.Application/Interfaces/ICourseServiceClient.cs
@@ -1,10 +1,13 @@
 ﻿using CourseModule.Application.UseCases.Courses.Contracts;
+using CourseModule.Domain.Entitites;
+using System.Linq.Expressions;
 
 namespace CourseModule.Application.Interfaces;
 
 public interface ICourseServiceClient
 {
     Task<Result<bool>> IsCourseAvailable(Guid courseId);
-    Task<Result<CourseResponseDto>?> GetCourseByIdAsync(Guid courseId);
+    Task<Result<CourseResponseDto?>> GetCourseByIdAsync(Guid courseId);
+    Task<Result<CourseResponseDto?>> GetCourse(Expression<Func<CourseEntity, bool>> predicate);
     Task<bool> IsStudentExistInCourseAsync(Guid courseId, Guid studentId);
 }
