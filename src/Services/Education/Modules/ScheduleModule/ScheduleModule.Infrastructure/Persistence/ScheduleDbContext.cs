8c772853db890a5dcb1a382e5c34889ecb73fbc3
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 10:55:31 +0500

    Proj implements Ef SqlServer

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Infrastructure/Persistence/ScheduleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScheduleModule.Infrastructure.EfConfigurations;
+using ScheduleModule.Infrastructure.Extensions;
+
+namespace ScheduleModule.Infrastructure.Persistence;
+
+public class ScheduleDbContext : DbContext
+{
+    public ScheduleDbContext(DbContextOptions<ScheduleDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new LessonScheduleConfiguration());
+
+        modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+    }
+}
