d4fdea5b1d2bebf939b3074cc584d971a5c4bb09
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 12:17:39 +0500

    DI Configs

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Orchestration/Extensions/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ScheduleModule.Orchestration.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddScheduleModuleOrchestration(
+        this IServiceCollection services)
+    {
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(AssemblyReference.Assembly);
+        });
+
+        return services;
+    }
+}
