38f5790fcd8f2e08326747c469cdee69955f68d7
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 10:53:15 +0500

    Schedule Entity For Lesson

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Domain/Entities/LessonScheduleEntity.cs
@@ -0,0 +1,35 @@
+﻿using SharedKernel.Domain.Primitives;
+
+namespace ScheduleModule.Domain.Entities;
+
+public class LessonScheduleEntity : Entity
+{
+    private LessonScheduleEntity(
+        Guid lessonId)
+    {
+        LessonId = lessonId;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public Guid LessonId { get; private set; }
+    public List<Guid> AbsentStudents { get; private set; } = new();
+    public DateTime CreatedAt { get; private set; }
+
+    public static Result<LessonScheduleEntity> Create(Guid lessonId)
+    {
+        return Result.Success(new LessonScheduleEntity(lessonId));
+    }
+
+    public void MarkStudentsAbsent(List<Guid> studentIds)
+    {
+        foreach (var student in studentIds)
+            if (!AbsentStudents.Contains(student))
+                AbsentStudents.Add(student);
+    }
+
+    public void MarkStudentsPresent(List<Guid> studentIds)
+    {
+        foreach (var student in studentIds)
+            AbsentStudents.Remove(student);
+    }
+}
