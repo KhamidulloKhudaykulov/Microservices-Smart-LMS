6f739fc7a7f2310398979becefa65872e8cf4e86
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 10:55:16 +0500

    EfConfig For Entity

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Infrastructure/EfConfigurations/LessonScheduleConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ScheduleModule.Domain.Entities;
+
+namespace ScheduleModule.Infrastructure.EfConfigurations;
+
+public class LessonScheduleConfiguration : IEntityTypeConfiguration<LessonScheduleEntity>
+{
+    public void Configure(EntityTypeBuilder<LessonScheduleEntity> builder)
+    {
+        builder.ToTable("schedule_lesson");
+    }
+}
