96f29bbbd5515a41abe469b2b91ad252c1b04675
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 12:17:32 +0500

    Absent Students Dto

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Orchestration/Dtos/AbsentStudentResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ScheduleModule.Orchestration.Dtos;
+
+public record AbsentStudentResponseDto(
+    string Fullname,
+    string Phonenumber,
+    string Email,
+    string Coursename,
+    string LessonTheme,
+    DateTime AbsetntsAt);
