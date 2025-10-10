acd25f51c468bbfadc3b448fa9aa71360d1ee149
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 15:18:37 +0500

    Api Endpoints For Student Caching Get Service

---
+++ src/Gateway/CacheGateway/CacheGateway.Api/Controllers/Caching.StudentController.cs
@@ -0,0 +1,24 @@
+﻿using CacheGateway.Logic.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CacheGateway.Api.Controllers;
+
+[ApiController]
+[Route("api/cache/students")]
+public class StudentCachingController : ControllerBase
+{
+    private readonly IStudentCacheService _studentCacheService;
+
+    public StudentCachingController(IStudentCacheService studentCacheService)
+    {
+        _studentCacheService = studentCacheService;
+    }
+
+    [HttpGet("verify/{id}")]
+    public async Task<IActionResult> VerifyExistStudent(Guid id)
+    {
+        var response = await _studentCacheService.VerifyExistStudentById(id);
+
+        return Ok(response);
+    }
+}
