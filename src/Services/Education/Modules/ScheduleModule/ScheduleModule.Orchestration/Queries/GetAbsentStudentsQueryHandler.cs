dd95a902f8e5d80e47e0a3edbfacbe5cdb17e8c5
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-10 12:18:01 +0500

    Orchestration Query For Getting Absent Students

---
+++ src/Services/Education/Modules/ScheduleModule/ScheduleModule.Orchestration/Queries/GetAbsentStudentsQueryHandler.cs
@@ -0,0 +1,60 @@
+﻿using CourseModule.Application.Interfaces;
+using LessonModule.Application.Interfaces;
+using MediatR;
+using ScheduleModule.Domain.Entities;
+using ScheduleModule.Domain.Repositories;
+using ScheduleModule.Orchestration.Dtos;
+using SharedKernel.Application.Abstractions.Messaging;
+using StudentIntegration.Application.InterfaceBridges;
+using System.Collections.Generic;
+
+namespace ScheduleModule.Orchestration.Queries;
+
+public record GetAbsentStudentsQuery(
+    Guid LessonId) : IQuery<List<AbsentStudentResponseDto>>;
+
+public class GetAbsentStudentsQueryHandler(
+    IScheduleRepository<LessonScheduleEntity> _scheduleRepository,
+    IStudentServiceClient _studentServiceClient,
+    ICourseServiceClient _courseServiceClient,
+    ILessonServiceClient _lessonServiceClient) : IQueryHandler<GetAbsentStudentsQuery, List<AbsentStudentResponseDto>>
+{
+    public async Task<Result<List<AbsentStudentResponseDto>>> Handle(GetAbsentStudentsQuery request, CancellationToken cancellationToken)
+    {
+        var details = await _scheduleRepository
+            .SelectAsync(s => s.LessonId == request.LessonId);
+
+        if (details is null)
+            return Result.Failure<List<AbsentStudentResponseDto>>(new Error(
+                code: "Lesson.NotFound",
+                message: "Lesson was not found"));
+
+        var absentStudents = details.AbsentStudents;
+
+        if (absentStudents is null)
+            return Result.Failure<List<AbsentStudentResponseDto>>(new Error(
+                code: "AbsetStudents.NotFound",
+                message: "Absent students was not found in this Lesson"));
+
+        var lesson = await _lessonServiceClient.GetLessonByIdAsync(request.LessonId);
+
+        var studentTask = _studentServiceClient.GetStudentsByIds(absentStudents);
+        var courseTask = _courseServiceClient.GetCourseByIdAsync(lesson!.CourseId);
+
+        await Task.WhenAll(studentTask, courseTask);
+
+        var students = await studentTask;
+        var course = await courseTask;
+
+        var result = students.Select(s => new AbsentStudentResponseDto(
+            Fullname: s.FullName,
+            Phonenumber: s.PhoneNumber,
+            LessonTheme: lesson.Theme,
+            Email: s.Email,
+            Coursename: course.Value!.Name,
+            AbsetntsAt: details.CreatedAt))
+            .ToList();
+
+        return Result.Success(result);
+    }
+}
