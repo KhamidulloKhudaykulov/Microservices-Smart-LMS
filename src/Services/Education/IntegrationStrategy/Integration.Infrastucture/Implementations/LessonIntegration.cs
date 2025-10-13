17865d2cac43999d1747ac9729b852539acfd427
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 10:18:20 +0500

    Base Integration Class

---
+++ src/Services/Education/IntegrationStrategy/Integration.Infrastucture/Implementations/LessonIntegration.cs
@@ -0,0 +1,35 @@
+﻿using Integration.Logic.Abstractions;
+using Integration.Logic.Dtos;
+using LessonModule.Domain.Repositories;
+
+namespace Integration.Infrastucture.Implementations;
+
+public class LessonIntegration : ILessonIntegration
+{
+    private readonly ILessonRepository _lessonRepository;
+
+    public LessonIntegration(ILessonRepository lessonRepository)
+    {
+        _lessonRepository = lessonRepository;
+    }
+
+    public async Task<bool> ChechExistLessonByIdAsync(Guid lessonId)
+    {
+        var existLesson = await _lessonRepository.SelectByIdAsync(lessonId);
+        return existLesson is not null;
+    }
+
+    public async Task<LessonResponseDto?> GetLessonByIdAsync(Guid lessonId)
+    {
+        var existLesson = await _lessonRepository.SelectByIdAsync(lessonId);
+
+        if (existLesson is null)
+            return null;
+
+        return new LessonResponseDto(
+            existLesson.CourseId,
+            existLesson.Theme.Value,
+            existLesson.Date
+        );
+    }
+}
