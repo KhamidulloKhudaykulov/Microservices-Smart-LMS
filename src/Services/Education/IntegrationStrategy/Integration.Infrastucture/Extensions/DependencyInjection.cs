2501ea5daa0d626b3015d205ea02cc41db696b9e
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 10:18:03 +0500

    DI Register

---
+++ src/Services/Education/IntegrationStrategy/Integration.Infrastucture/Extensions/DependencyInjection.cs
@@ -0,0 +1,51 @@
+﻿using Integration.Infrastucture.Configurations;
+using Integration.Infrastucture.Implementations;
+using Integration.Logic.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Integration.Infrastucture.Extensions;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddIntegrationInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<RedisSettings>(configuration.GetSection("redis"));
+
+        var redisSettings = configuration.GetSection("redis").Get<RedisSettings>();
+        services.AddSingleton(redisSettings!);
+
+        IConnectionMultiplexer? connectionMultiplexer = null;
+
+        if (redisSettings!.IsEnabled && !string.IsNullOrWhiteSpace(redisSettings.Host))
+        {
+            try
+            {
+                connectionMultiplexer = ConnectionMultiplexer.Connect(redisSettings.Host!);
+                services.AddSingleton<IConnectionMultiplexer>(connectionMultiplexer);
+
+                services.AddScoped<LessonIntegration>();
+                services.AddScoped<ILessonIntegration>(sp =>
+                {
+                    var inner = sp.GetRequiredService<LessonIntegration>();
+                    var redis = sp.GetRequiredService<IConnectionMultiplexer>();
+                    return new LessonRedisIntegrationDecorator(inner, redis);
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Redis connection failed: {ex.Message}");
+
+                services.AddScoped<ILessonIntegration, LessonIntegration>();
+            }
+        }
+        else
+        {
+            services.AddScoped<ILessonIntegration, LessonIntegration>();
+        }
+
+        return services;
+    }
+}
