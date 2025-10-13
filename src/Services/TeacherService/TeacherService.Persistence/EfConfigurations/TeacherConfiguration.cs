7e2fffa1ce0c711d8d19198a5fb0b941563df1ac
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 15:37:50 +0500

    EF config

---
+++ src/Services/TeacherService/TeacherService.Persistence/EfConfigurations/TeacherConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TeacherService.Domain.Aggregates;
+
+namespace TeacherService.Persistence.EfConfigurations;
+
+public class TeacherConfiguration : IEntityTypeConfiguration<Teacher>
+{
+    public void Configure(EntityTypeBuilder<Teacher> builder)
+    {
+        builder.ToTable("teachers");
+    }
+}
