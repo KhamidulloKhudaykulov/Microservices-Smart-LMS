3b3c05a35fda65e9e44e4235c46e07bdc15bcc34
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 15:36:36 +0500

    Aggregate entity

---
+++ src/Services/TeacherService/TeacherService.Domain/Aggregates/Teacher.cs
@@ -0,0 +1,36 @@
+﻿using SharedKernel.Domain.Primitives;
+
+namespace TeacherService.Domain.Aggregates;
+
+public class Teacher : AggregateRoot
+{
+    private Teacher(
+        Guid id,
+        string name,
+        string surname,
+        string email,
+        string phoneNumber) 
+        : base(id)
+    {
+        Name = name;
+        Surname = surname;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+
+    public string Name { get; private set; }
+    public string Surname { get; private set; }
+    public string Email { get; private set; }
+    public string PhoneNumber { get; private set; }
+
+    public static Result<Teacher> Create(
+        Guid id,
+        string name,
+        string surname,
+        string email,
+        string phoneNumber)
+    {
+        return Result.Success(new Teacher(
+            id, name, surname, email, phoneNumber));
+    }
+}
