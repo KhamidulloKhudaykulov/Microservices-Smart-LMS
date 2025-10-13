2da02a9a802416c0bf4a45b41f58ab946da0b15a
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 15:36:46 +0500

    DI extensions

---
+++ src/Services/TeacherService/TeacherService.Persistence/Extensions/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TeacherService.Persistence.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<TeacherServiceDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultStudentDbConnection"));
+        });
+
+        return services;
+    }
+}
