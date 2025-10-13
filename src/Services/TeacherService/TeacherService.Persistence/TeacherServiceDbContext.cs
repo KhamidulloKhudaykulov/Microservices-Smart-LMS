77a3626321a3c4df0acf25dd8ea466acad133419
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 15:37:42 +0500

    Db Context (SQL Server)

---
+++ src/Services/TeacherService/TeacherService.Persistence/TeacherServiceDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeacherService.Persistence.EfConfigurations;
+using TeacherService.Persistence.Extensions;
+
+namespace TeacherService.Persistence;
+
+public class TeacherServiceDbContext
+    : DbContext
+{
+    public TeacherServiceDbContext(DbContextOptions<TeacherServiceDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TeacherConfiguration());
+
+        modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+    }
+}
