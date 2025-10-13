113d99710eb3d2298bb4e8511f48cd754e1dbff3
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 15:38:01 +0500

    Repo abstraction

---
+++ src/Services/TeacherService/TeacherService.Domain/Repositories/ITecherRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using TeacherService.Domain.Aggregates;
+
+namespace TeacherService.Domain.Repositories;
+
+public interface ITecherRepository
+{
+    Task<Teacher> InsertAsync(Teacher teacher);
+    Task<Teacher> UpdateAsync(Teacher teacher);
+    Task<Teacher> DeleteAsync(Teacher teacher);
+    Task<Teacher?> SelectByIdAsync(Guid id);
+    Task<Teacher?> SelectAsync(Expression<Func<Teacher, bool>> predicate);
+    Task<IEnumerable<Teacher>> SelectAllAsync(Expression<Func<Teacher, bool>>? predicate = null);
+    IQueryable<Teacher> SelectAsQueryable(Expression<Func<Teacher, bool>>? predicate = null);
+}
