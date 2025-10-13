5280e5c471dbaeb38a4803b45e81ecfafd437a02
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 10:17:52 +0500

    Base Decorator

---
+++ src/Services/Education/IntegrationStrategy/Integration.Infrastucture/Decorators/BaseDecorator.cs
@@ -0,0 +1,12 @@
+﻿namespace Integration.Infrastucture.Decorators;
+
+public abstract class BaseDecorator<TService>
+    where TService : class
+{
+    protected readonly TService _inner;
+
+    protected BaseDecorator(TService inner)
+    {
+        _inner = inner ?? throw new ArgumentNullException(nameof(inner));
+    }
+}
