94347bcd5fd350b7c24452e3ac2bc1bcb79dcef8
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 11:40:50 +0500

    Implement

---
+++ src/Services/Education/IntegrationStrategy/Integration.Infrastucture/Implementations/CourseIntegration.cs
@@ -0,0 +1,59 @@
+﻿using CourseModule.Application.UseCases.Courses.Contracts;
+using CourseModule.Domain.Entitites;
+using CourseModule.Domain.Exceptions;
+using CourseModule.Domain.Repositories;
+using Integration.Logic.Abstractions;
+using System.Linq.Expressions;
+
+namespace CourseModule.Infrastructure.Services.Courses;
+
+public class CourseIntegration(
+    ICourseRepository _courseRepository)
+    : ICourseIntegration
+{
+    public async Task<Result<CourseResponseDto?>> GetCourse(Expression<Func<CourseEntity, bool>> predicate)
+    {
+        var course = await _courseRepository
+            .SelectAsync(predicate);
+
+        if (course is null)
+            return Results.NotFoundException<CourseResponseDto?>(CourseErrors.NotFound);
+
+        return new CourseResponseDto(course.Id, course.Name, course.StartsAt);
+    }
+
+    public async Task<Result<CourseResponseDto?>> GetCourseByIdAsync(Guid courseId)
+    {
+        var course = await _courseRepository
+            .SelectByIdAsync(courseId);
+
+        if (course is null)
+            return Results.NotFoundException<CourseResponseDto?>(CourseErrors.NotFound);
+
+        return new CourseResponseDto(course.Id, course.Name, course.StartsAt);
+    }
+
+    public async Task<Result<bool>> IsCourseAvailable(Guid courseId)
+    {
+        var course = await _courseRepository
+            .SelectByIdAsync(courseId);
+
+        if (course is null)
+            return Results.NotFoundException<bool>(CourseErrors.NotFound);
+
+        return Result.Success(true);
+    }
+
+    public async Task<bool> IsStudentExistInCourseAsync(Guid courseId, Guid studentId)
+    {
+        var course = await _courseRepository
+            .SelectByIdAsync(courseId);
+
+        if (course is null)
+            return false;
+
+        var hasStudent = course.StudentIds.Contains(studentId);
+
+        return hasStudent;
+    }
+}
