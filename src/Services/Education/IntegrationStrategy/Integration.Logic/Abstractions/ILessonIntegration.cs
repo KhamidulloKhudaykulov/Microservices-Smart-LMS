86d0bcdb817af23eb50edf6525cf86d7a1e6fe92
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 10:19:08 +0500

    Abstraction For Communication Between Lesson Module

---
+++ src/Services/Education/IntegrationStrategy/Integration.Logic/Abstractions/ILessonIntegration.cs
@@ -0,0 +1,9 @@
+﻿using Integration.Logic.Dtos;
+
+namespace Integration.Logic.Abstractions;
+
+public interface ILessonIntegration
+{
+    Task<bool> ChechExistLessonByIdAsync(Guid lessonId);
+    Task<LessonResponseDto?> GetLessonByIdAsync(Guid lessonId);
+}
