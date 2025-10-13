7790854362a20b44f835905205ffe507820192dc
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-13 11:40:44 +0500

    Abstraction CourseIntegration

---
+++ src/Services/Education/IntegrationStrategy/Integration.Logic/Abstractions/ICourseIntegration.cs
@@ -0,0 +1,13 @@
+﻿using CourseModule.Application.UseCases.Courses.Contracts;
+using CourseModule.Domain.Entitites;
+using System.Linq.Expressions;
+
+namespace Integration.Logic.Abstractions;
+
+public interface ICourseIntegration
+{
+    Task<Result<bool>> IsCourseAvailable(Guid courseId);
+    Task<Result<CourseResponseDto?>> GetCourseByIdAsync(Guid courseId);
+    Task<Result<CourseResponseDto?>> GetCourse(Guid courseId, Expression<Func<CourseEntity, bool>> predicate);
+    Task<bool> IsStudentExistInCourseAsync(Guid courseId, Guid studentId);
+}
