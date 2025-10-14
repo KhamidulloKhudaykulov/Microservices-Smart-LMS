0f20558bf92a96c7bcdfe9a9dfcbe81cb1695191
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-14 10:40:35 +0500

    DbContextFactory

---
+++ src/Services/TeacherService/TeacherService.Persistence/TeacherServiceDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+namespace TeacherService.Persistence;
+
+public class TeacherServiceDbContextFactory : IDesignTimeDbContextFactory<TeacherServiceDbContext>
+{
+    public TeacherServiceDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultTeacherDbConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<TeacherServiceDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new TeacherServiceDbContext(optionsBuilder.Options);
+    }
+}
