322ac7ef165daa0f9315db1c957bfe0b825e9800
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-14 10:40:49 +0500

    API fixed

---
+++ src/Services/TeacherService/TeacherService.Api/Program.cs
@@ -0,0 +1,21 @@
+using TeacherService.Persistence.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddPersistence(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
