b81fe2497f7ecaff6a784dbbba708c0f785bb67c
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-15 13:49:51 +0500

    DI Extension

---
+++ src/Services/TeacherService/TeacherService.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TeacherService.Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(AssemblyReference.Assembly);
+        });
+
+        return services;
+    }
+}
