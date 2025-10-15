5be0e5a6dc8c7f6915786c8d9e20b89ca3808c39
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-15 13:48:41 +0500

    UoW

---
+++ src/Services/TeacherService/TeacherService.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using SharedKernel.Domain.Repositories;
+
+namespace TeacherService.Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly TeacherServiceDbContext _dbContext;
+
+    public UnitOfWork(TeacherServiceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
