d455c3dcab1b0c00f3bff9e2343664b010ee4f93
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-15 13:49:58 +0500

    Base queries

---
+++ src/Services/TeacherService/TeacherService.Application/UseCases/Teachers/Queries/GetAllTeachersQueryHandler.cs
@@ -0,0 +1,50 @@
+﻿using SharedKernel.Application.Abstractions.Messaging;
+using TeacherService.Application.UseCases.Teachers.Contracts;
+using TeacherService.Domain.Repositories;
+
+namespace TeacherService.Application.UseCases.Teachers.Queries;
+
+public record GetAllTeachersQuery(int Page = 1, int PageSize = 10)
+    : IQuery<PagedList<TeacherResponseDto>>;
+
+public class GetAllTeachersQueryHandler(
+    ITecherRepository _teacherRepository)
+    : IQueryHandler<GetAllTeachersQuery, PagedList<TeacherResponseDto>>
+{
+    public async Task<Result<PagedList<TeacherResponseDto>>> Handle(
+        GetAllTeachersQuery request,
+        CancellationToken cancellationToken)
+    {
+        var teachers = await _teacherRepository.SelectAllAsync();
+        if (teachers is null || !teachers.Any())
+            return Result.Failure<PagedList<TeacherResponseDto>>(new Error(
+                code: "Teacher.NotFound",
+                message: "No teachers found."));
+
+        var totalCount = teachers.Count();
+        var skip = (request.Page - 1) * request.PageSize;
+
+        var teacherDtos = teachers
+            .Skip(skip)
+            .Take(request.PageSize)
+            .Select(t => new TeacherResponseDto(
+                t.Id,
+                t.Name.Value,
+                t.Surname.Value,
+                t.Email.Value,
+                t.PhoneNumber.Value,
+                t.Status.ToString()))
+            .ToList();
+
+        var pagedItems = teacherDtos
+            .ToList();
+
+        var pagedList = new PagedList<TeacherResponseDto>(
+            pagedItems,
+            totalCount,
+            request.Page,
+            request.PageSize);
+
+        return Result.Success(pagedList);
+    }
+}
---
+++ src/Services/TeacherService/TeacherService.Application/UseCases/Teachers/Queries/GetTeacherByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using SharedKernel.Application.Abstractions.Messaging;
+using TeacherService.Application.UseCases.Teachers.Contracts;
+using TeacherService.Domain.Repositories;
+
+namespace TeacherService.Application.UseCases.Teachers.Queries;
+
+public record GetTeacherByIdQuery(Guid Id) : IQuery<TeacherResponseDto>;
+
+public class GetTeacherByIdQueryHandler(
+    ITecherRepository _teacherRepository)
+    : IQueryHandler<GetTeacherByIdQuery, TeacherResponseDto>
+{
+    public async Task<Result<TeacherResponseDto>> Handle(
+        GetTeacherByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var teacher = await _teacherRepository.SelectByIdAsync(request.Id);
+        if (teacher is null)
+            return Result.Failure<TeacherResponseDto>(new Error(
+                code: "Teacher.NotFound",
+                message: $"Teacher with ID={request.Id} was not found"));
+
+        var response = new TeacherResponseDto(
+            teacher.Id,
+            teacher.Name.Value,
+            teacher.Surname.Value,
+            teacher.Email.Value,
+            teacher.PhoneNumber.Value,
+            teacher.Status.ToString()
+        );
+
+        return Result.Success(response);
+    }
+}
