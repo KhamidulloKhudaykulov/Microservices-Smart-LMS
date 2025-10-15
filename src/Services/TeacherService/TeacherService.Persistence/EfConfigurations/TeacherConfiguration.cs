ee6ae15ee1a88a3deead853d13c73adf168eab2d
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-15 13:48:34 +0500

    Ef Config for teacher aggregate

--- src/Services/TeacherService/TeacherService.Persistence/EfConfigurations/TeacherConfiguration.cs
+++ src/Services/TeacherService/TeacherService.Persistence/EfConfigurations/TeacherConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TeacherService.Domain.Aggregates;
+using TeacherService.Domain.ValueObjects.Teachers;
 
 namespace TeacherService.Persistence.EfConfigurations;
 
@@ -9,5 +10,17 @@ public class TeacherConfiguration : IEntityTypeConfiguration<Teacher>
     public void Configure(EntityTypeBuilder<Teacher> builder)
     {
         builder.ToTable("teachers");
+
+        builder.Property(x => x.Name)
+            .HasConversion(x => x!.Value, v => TeacherName.Create(v).Value);
+
+        builder.Property(x => x.Email)
+            .HasConversion(x => x!.Value, v => Email.Create(v).Value);
+
+        builder.Property(x => x.Surname)
+            .HasConversion(x => x!.Value, v => TeacherSurname.Create(v).Value);
+
+        builder.Property(x => x.PhoneNumber)
+            .HasConversion(x => x!.Value, v => PhoneNumber.Create(v).Value);
     }
 }
