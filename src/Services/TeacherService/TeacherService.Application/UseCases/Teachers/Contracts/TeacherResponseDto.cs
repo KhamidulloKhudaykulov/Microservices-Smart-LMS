94a374ec59d6dcb1b5a1e3619ac810a0a6958c60
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-15 13:49:33 +0500

    Response DTO

---
+++ src/Services/TeacherService/TeacherService.Application/UseCases/Teachers/Contracts/TeacherResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TeacherService.Application.UseCases.Teachers.Contracts;
+
+public record TeacherResponseDto(
+    Guid Id,
+    string Name,
+    string Surname,
+    string Email,
+    string PhoneNumber,
+    string Status);
