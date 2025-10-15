476f0d74c39c28e796126de330d55054a63d2ccb
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2025-10-15 13:49:25 +0500

    wraps the list of response

---
+++ src/Services/TeacherService/TeacherService.Application/UseCases/PagedList.cs
@@ -0,0 +1,18 @@
+﻿namespace TeacherService.Application.UseCases;
+
+public class PagedList<T>
+{
+    public IReadOnlyCollection<T> Items { get; }
+    public int TotalCount { get; }
+    public int Page { get; }
+    public int PageSize { get; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+
+    public PagedList(IReadOnlyCollection<T> items, int totalCount, int page, int pageSize)
+    {
+        Items = items;
+        TotalCount = totalCount;
+        Page = page;
+        PageSize = pageSize;
+    }
+}
